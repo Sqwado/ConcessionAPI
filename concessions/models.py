094dcdc0e44c72644f92d577bfd8e21acb2a8695
Sqwado <mateoluque@aol.com>
2024-12-31 13:46:16 +0100

    Définition des modèles Concession et Vehicule

--- concessions/models.py
+++ concessions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Concession(models.Model):
+    nom = models.CharField(max_length=255)
+    numero_siret = models.CharField(max_length=14, null=True, blank=True) #Facultatif
+    code_postal = models.CharField(max_length=5)
+    adresse = models.TextField()
+
+    def __str__(self):
+        return self.nom
+
+class Vehicule(models.Model):
+    marque = models.CharField(max_length=255)
+    modele = models.CharField(max_length=255)
+    chevaux = models.IntegerField()
+    immatriculation = models.CharField(max_length=10, unique=True)
+    date_mise_en_service = models.DateField()
+    concession = models.ForeignKey(Concession, related_name='vehicules', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.marque} {self.modele} ({self.immatriculation})"
