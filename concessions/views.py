02aa42e4bf497ea0ec26fea20df155d8aac38a03
Sqwado <mateoluque@aol.com>
2024-12-31 13:48:28 +0100

    Ajout des ViewSets pour Concession et Vehicule

--- concessions/views.py
+++ concessions/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Concession, Vehicule
+from .serializers import ConcessionSerializer, VehiculeSerializer
+
+class ConcessionViewSet(viewsets.ModelViewSet):
+    queryset = Concession.objects.all()
+    serializer_class = ConcessionSerializer
+
+class VehiculeViewSet(viewsets.ModelViewSet):
+    queryset = Vehicule.objects.all()
+    serializer_class = VehiculeSerializer
